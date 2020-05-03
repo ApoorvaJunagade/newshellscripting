2f2a1221ebfa59c542e1733a3bd6a8958a1f4a4e
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-03 12:30:08 +0530

    USECASE6: result4 displayed

--- add.sh
+++ add.sh
@@ -5,4 +5,6 @@ read -p " enter third number" num3
 echo "result1:"$(( $num1 + (($num2 * $num3)) ))
 echo "result2:"$(( (($num1 *$num2)) + $num3 ))
 echo "result3:"$(( $num3 +(($num1 / $num2 )) ))
+echo "result4:"$(( (($num1 %$num2)) + $num3 ))
+
 
