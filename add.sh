476e442b6fcd53a123dc36c118611c917451eeb4
ApoorvaJunagade <apoorva.junagade@gmail.com>
2020-05-03 12:21:29 +0530

    USECASE5: result3 displayed

--- add.sh
+++ add.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
-read -p " enter number" num
-read -p " enter second number" num1
+read -p " enter number" num1
+read -p " enter second number" num2
 read -p " enter third number" num3
-echo "result1:"$(( $num + (($num1 * $num3)) ))
-echo "result2:"$(( (($num *$num1)) + $num3 ))
+echo "result1:"$(( $num1 + (($num2 * $num3)) ))
+echo "result2:"$(( (($num1 *$num2)) + $num3 ))
+echo "result3:"$(( $num3 +(($num1 / $num2 )) ))
+
